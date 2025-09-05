ca3dc3e8796b7834f713895705119b2b469d1a83
Ivy Case <ivycase@berkeley.edu>
2025-09-05 00:50:20 -0700

    feat: add test tweener script

---
+++ Assets/Suitors/MoveToScript.cs
@@ -0,0 +1,22 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class MoveToScript : MonoBehaviour
+{
+
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("The animation's transition duration.")]
+    private float moveDuration = 1.0f;
+
+    [SerializeField]
+    [Tooltip("The target position for movement.")]
+    private Vector2 targetPosition = new Vector2(0, 0);
+    #endregion
+
+
+    void Start()
+    {
+        transform.DOMove(targetPosition, moveDuration).SetEase(Ease.OutBack);
+    }
+}
