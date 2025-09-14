a6ae41ebffb07578c871aa7d1651604e6c6d810c
Ivy Case <ivycase@berkeley.edu>
2025-09-14 03:16:54 -0700

    feat: curtain transition animation!

--- Assets/UI/CloseOpenCurtainsScript.cs
+++ Assets/UI/CloseOpenCurtainsScript.cs
@@ -4,28 +4,24 @@ using Yarn.Unity;
 
 public class CloseOpenCurtainsScript : MonoBehaviour
 {
-    public GameObject dialogueText;
-    public GameObject monologueText;
 
-    public SpriteRenderer targetSprite;
-    public Sprite curtainsOpen;
-    public Sprite curtainsClosed;
+    public Animator curtainsAnimator;
 
     [YarnCommand("open")]
     public void OpenCurtains()
     {
-        dialogueText.GetComponent<TextMeshProUGUI>().text = "";
-        monologueText.GetComponent<CanvasRenderer>().SetAlpha(0);
-        dialogueText.GetComponent<CanvasRenderer>().SetAlpha(1);
-        targetSprite.sprite = curtainsOpen;
+        curtainsAnimator.SetTrigger("open");
     }
 
     [YarnCommand("close")]
     public void CloseCurtains()
     {
-        monologueText.GetComponent<TextMeshProUGUI>().text = "";
-        monologueText.GetComponent<CanvasRenderer>().SetAlpha(1);
-        dialogueText.GetComponent<CanvasRenderer>().SetAlpha(0);
-        targetSprite.sprite = curtainsClosed;
+        curtainsAnimator.SetTrigger("close");
+    }
+
+    [YarnCommand("transition")]
+    public void TransCurtains()
+    {
+        curtainsAnimator.SetTrigger("transition");
     }
 }
