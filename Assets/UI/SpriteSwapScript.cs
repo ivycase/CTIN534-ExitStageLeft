b27035068bba95208171136084682c2ebba81d8d
Ivy Case <ivycase@berkeley.edu>
2025-09-14 23:07:09 -0700

    feat: change_sprite command, move sprite locations, suitor sprites

--- Assets/UI/SpriteSwapScript.cs
+++ Assets/UI/SpriteSwapScript.cs
@@ -3,7 +3,7 @@ using Yarn.Unity;
 
 public class SpriteSwapScript : MonoBehaviour
 {
-    [YarnCommand("sprite_swap")]
+    [YarnCommand("change_sprite")]
     public void Swap(string spriteName)
     {
         GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(string.Format("Sprites/{0}", spriteName));
